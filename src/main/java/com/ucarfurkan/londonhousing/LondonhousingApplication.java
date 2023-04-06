8255e3ed0edc27be56ad228628c8399acb52054f
ucarfurkan <furkanucar011@gmail.com>
2023-04-06 23:41:55 +0300

    initialization of Spring project

---
+++ src/main/java/com/ucarfurkan/londonhousing/LondonhousingApplication.java
@@ -0,0 +1,13 @@
+package com.ucarfurkan.londonhousing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LondonhousingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LondonhousingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ucarfurkan/londonhousing/LondonhousingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ucarfurkan.londonhousing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LondonhousingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
